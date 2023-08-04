5de9292c755c012ce02099cf48f52db5d7952618
Arafat04H09 <arafat.hossain@stonybrook.edu>
2023-08-04 19:48:46 -0400

    created react project

---
+++ my-react-app/src/Components/LandingPage.js
@@ -0,0 +1,24 @@
+// src/components/LandingPage.js
+
+import React from 'react';
+
+const LandingPage = () => {
+  return (
+    <div className="landing-page">
+      <header>
+        <h1>Welcome to Your Spotify Statistics!</h1>
+        <p>Track and visualize your Spotify data like never before.</p>
+      </header>
+      <main>
+        <button className="login-button">
+          Login with Spotify  
+        </button>
+      </main>
+      <footer>
+        <p>Powered by Spotify API and AWS</p>
+      </footer>
+    </div>
+  );
+};
+
+export default LandingPage;
