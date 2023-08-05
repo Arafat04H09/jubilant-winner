831a7391b05e854383abc0ade8fcdae76bf59a08
bloopgoop <zhu.kevin12@gmail.com>
2023-08-05 18:33:29 -0400

    Added about

--- my-react-app/src/Components/AboutPage.js
+++ my-react-app/src/Components/AboutPage.js
@@ -1,11 +1,22 @@
-// AboutPage.js
 import React from 'react';
+import './About.css';
 
 const AboutPage = () => {
   return (
-    <div className="about-page">
-      <h2>About Page</h2>
-      {/* ... Your about page content ... */}
+    <div className="about-container">
+      <h2 className="about-heading">About</h2>
+      <p className="about-paragraph">
+        Welcome to (NAME)! This is a web application built with React to provide Spotify statistics and insights to users.
+      </p>
+      <p className="about-paragraph">
+        With our app, you can track and visualize your Spotify data. We use the Spotify Web API to fetch user data, including top tracks, playlists, and more.
+      </p>
+      <p className="about-paragraph">
+        To get started, you can log in with your Spotify account and authorize our app to access your Spotify data. Once logged in, you'll be able to view personalized statistics and visualize your music listening habits.
+      </p>
+      <p className="about-paragraph">
+        Thank you for using our App!
+      </p>
     </div>
   );
 };
